8684536043b1edc49fdae2e50f7006dde418816d
Alfan Jauhari <hi@alfanjauhari.com>
2023-01-17 16:46:35 +0700

    feat: add Grid component

--- packages/components/core/src/Grid/Grid.spec.tsx
+++ packages/components/core/src/Grid/Grid.spec.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 
-import Grid from './Grid';
+import { Grid } from './Grid';
 
 describe('Grid', () => {
   it('should render successfully', () => {
