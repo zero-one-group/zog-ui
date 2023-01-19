e41775714c9366abf70166d92640e3100bb5732d
Alfan Jauhari <hi@alfanjauhari.com>
2023-01-19 08:20:24 +0700

    feat: create heading component

--- packages/components/core/src/Heading/Heading.spec.tsx
+++ packages/components/core/src/Heading/Heading.spec.tsx
@@ -1,10 +1,20 @@
 import { render } from '@testing-library/react';
 
-import Heading from './Heading';
+import { Heading } from './Heading';
 
 describe('Heading', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Heading />);
     expect(baseElement).toBeTruthy();
+  });
+
+  it('should render polymorphic component', () => {
+    const { getByRole } = render(
+      <Heading as="a" href="https://google.com">
+        It's Anchor Component
+      </Heading>
+    );
+
+    expect(getByRole('link')).toBeInTheDocument();
   });
 });
