21bd8a1e53e10a6c2e0392967bc44759d2619600
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2023-01-25 13:03:55 +0700

    feat: generate layout component

---
+++ packages/components/core/src/Layout/Layout.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface LayoutProps {}
+
+export function Layout(props: LayoutProps) {
+  return (
+    <div>
+      <h1>This is Layout!</h1>
+    </div>
+  );
+}
+
+export default Layout;
