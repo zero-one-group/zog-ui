442d918fa8dba125d752848ea33f34410c415f7f
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2023-01-27 10:55:40 +0700

    feat: generate AutoComplete component web

---
+++ packages/components/core/src/AutoComplete/AutoComplete.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface AutoCompleteProps {}
+
+export function AutoComplete(props: AutoCompleteProps) {
+  return (
+    <div>
+      <h1>Welcome to AutoComplete!</h1>
+    </div>
+  );
+}
+
+export default AutoComplete;
