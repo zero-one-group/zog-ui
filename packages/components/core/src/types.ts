204f13f9d16e9c5b961bfe3b984d021792b4d7e0
Alfan Jauhari <hi@alfanjauhari.com>
2023-01-28 19:06:38 +0700

    feat: initialize Menu component

--- packages/components/core/src/types.ts
+++ packages/components/core/src/types.ts
@@ -5,18 +5,30 @@ import {
   PropsWithChildren,
 } from 'react';
 
+/**
+ * @deprecated Use `asChild` instead
+ */
 export type AsProps<T extends ElementType> = {
   as?: T;
 };
 
+/**
+ * @deprecated Use `asChild` instead
+ */
 export type KeyPropsToOmit<T extends ElementType, P> = keyof (AsProps<T> & P);
 
+/**
+ * @deprecated Use `asChild` instead
+ */
 export type PolymorphicComponentProps<
   T extends ElementType,
   Props = unknown
 > = PropsWithChildren<AsProps<T> & Props> &
   Omit<ComponentPropsWithoutRef<T>, KeyPropsToOmit<T, Props>>;
 
+/**
+ * @deprecated Use `asChild` instead
+ */
 export type PolymorphicRef<T extends ElementType> =
   ComponentPropsWithRef<T>['ref'];
 
@@ -26,3 +38,5 @@ export type PolymorphicComponentPropsWithRef<
 > = PolymorphicComponentProps<T, Props> & { ref?: PolymorphicRef<T> };
 
 export type MergeProps<T, P> = Omit<T, keyof P> & P;
+
+export type RemoveAsProps<Props> = Omit<Props, 'as'>;
