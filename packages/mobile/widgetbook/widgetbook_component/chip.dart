c741bb94dd743cd71becd8cac4e0aeff6ae2ddf7
Wisnu G Saputra <wisnuwiry@gmail>
2023-02-02 13:57:06 +0700

    chore(mobile): Implement widgetbook chip

---
+++ packages/mobile/widgetbook/widgetbook_component/chip.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:zero_ui_mobile/zero_ui_mobile.dart';
+
+import '../utils.dart';
+
+WidgetbookComponent chipWidgetbookComponent = WidgetbookComponent(
+  name: 'Chip',
+  useCases: [
+    WidgetbookUseCase(
+      name: 'Filled without Cancel Icon',
+      builder: (context) => PreviewWidget(
+        child: ZeroChipFilled(
+          label: context.knobs
+              .text(label: 'Text Label', initialValue: 'Chip Label'),
+          disabled:
+              context.knobs.boolean(label: 'Disabled', initialValue: false),
+          onTap: () {},
+          size: context.knobs.options(label: 'Size', options: _sizeOptions),
+          avatar:
+              context.knobs.options(label: 'Avatar', options: _avatarOptions),
+        ),
+      ),
+    ),
+    WidgetbookUseCase(
+      name: 'Filled without with Icon',
+      builder: (context) => PreviewWidget(
+        child: ZeroChipFilled(
+          label: context.knobs
+              .text(label: 'Text Label', initialValue: 'Chip Label'),
+          disabled:
+              context.knobs.boolean(label: 'Disabled', initialValue: false),
+          onTap: () {},
+          size: context.knobs.options(label: 'Size', options: _sizeOptions),
+          avatar:
+              context.knobs.options(label: 'Avatar', options: _avatarOptions),
+          onTapCancel: () {},
+          cancelIcon: context.knobs
+              .options(label: 'Cancel Icon', options: _iconOptions),
+        ),
+      ),
+    ),
+    WidgetbookUseCase(
+      name: 'Outlined without Cancel Icon',
+      builder: (context) => PreviewWidget(
+        child: ZeroChipOutlined(
+          label: context.knobs
+              .text(label: 'Text Label', initialValue: 'Chip Label'),
+          disabled:
+              context.knobs.boolean(label: 'Disabled', initialValue: false),
+          onTap: () {},
+          size: context.knobs.options(label: 'Size', options: _sizeOptions),
+          avatar:
+              context.knobs.options(label: 'Avatar', options: _avatarOptions),
+        ),
+      ),
+    ),
+    WidgetbookUseCase(
+      name: 'Outlined without with Icon',
+      builder: (context) => PreviewWidget(
+        child: ZeroChipOutlined(
+          label: context.knobs
+              .text(label: 'Text Label', initialValue: 'Chip Label'),
+          disabled:
+              context.knobs.boolean(label: 'Disabled', initialValue: false),
+          onTap: () {},
+          size: context.knobs.options(label: 'Size', options: _sizeOptions),
+          avatar:
+              context.knobs.options(label: 'Avatar', options: _avatarOptions),
+          onTapCancel: () {},
+          cancelIcon: context.knobs
+              .options(label: 'Cancel Icon', options: _iconOptions),
+        ),
+      ),
+    ),
+  ],
+);
+
+List<Option<ZeroChipSize>> _sizeOptions = [
+  const Option(label: 'Small', value: ZeroChipSize.small),
+  const Option(label: 'Large', value: ZeroChipSize.large),
+];
+
+List<Option<IconData>> _iconOptions = [
+  const Option(label: 'Cancel', value: Icons.cancel),
+  const Option(label: 'Close', value: Icons.close_rounded),
+  const Option(label: 'Delete', value: Icons.delete),
+  const Option(label: 'Add', value: Icons.add),
+];
+
+List<Option<Widget?>> _avatarOptions = [
+  const Option(label: 'None', value: null),
+  Option(
+    label: 'Avatar',
+    value: ZeroAvatar.url(
+      'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?fit=crop&w=80&h=80&q=80',
+      size: AvatarSize.xs,
+    ),
+  ),
+];
