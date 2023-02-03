a86927555b1116db87c0432312fa7195803fd851
Wisnu G Saputra <wisnuwiry@gmail>
2023-02-03 13:58:11 +0700

    feat(mobile): Implement icons(ZeroIcons)

--- packages/mobile/lib/zero_ui_mobile.dart
+++ packages/mobile/lib/zero_ui_mobile.dart
@@ -78,6 +78,9 @@ export './components/switch/zero_switch.dart';
 export './components/switch/zero_switch_android.dart';
 export './components/switch/zero_switch_ios.dart';
 
+// icons
+export './components/icons/zero_icons.dart';
+
 // types
 export './types/button_radius_type.dart';
 export './types/button_group_type.dart';
