fdabb674f949d41a673db8f1bb6771b718861834
Muhammad Rijalul Kahfi <yanmi.inc@gmail.com>
2023-02-06 16:13:19 +0700

    feat: added widgetbook for datepicker

--- packages/mobile/widgetbook/lib/widgetbook_component/datepicker.dart
+++ packages/mobile/widgetbook/lib/widgetbook_component/datepicker.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:zero_ui_mobile/zero_ui_mobile.dart';
 
@@ -7,19 +8,29 @@ WidgetbookComponent datepickerWidgetbookComponent = WidgetbookComponent(
   name: 'Date Picker',
   useCases: [
     WidgetbookUseCase(
-      name: 'Date Picker',
-      builder: (context) => ZeroDatePickerDialog(
-        initialDate: DateTime.now(),
-        firstDate: DateTime.now().subtract(const Duration(days: 30)),
-        lastDate: DateTime.now().add(const Duration(days: 100)),
-      ),
-    ),
+        name: 'Date Picker',
+        builder: (context) => ZeroApp(
+              home: PreviewWidget(
+                child: ZeroButton.primary(
+                    text: 'Date Picker',
+                    onPressed: () async {
+                      showZeroDatePicker(
+                        context: context,
+                        initialDate: DateTime.now(),
+                        firstDate:
+                            DateTime.now().subtract(const Duration(days: 30)),
+                        lastDate: DateTime.now().add(const Duration(days: 100)),
+                      );
+                    }),
+              ),
+            )),
     WidgetbookUseCase(
       name: 'Date Rage Picker',
-      builder: (context) => ZeroDatePickerDialog(
-        initialDate: DateTime.now(),
-        firstDate: DateTime.now().subtract(const Duration(days: 30)),
-        lastDate: DateTime.now().add(const Duration(days: 100)),
+      builder: (context) => PreviewWidget(
+        child: ZeroDateRangePickerDialog(
+          firstDate: DateTime.now().subtract(const Duration(days: 30)),
+          lastDate: DateTime.now().add(const Duration(days: 100)),
+        ),
       ),
     ),
   ],
