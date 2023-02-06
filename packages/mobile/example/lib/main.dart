a57feb25dcea6cf5de4231ba75637706f8f2a094
Wisnu G Saputra <wisnuwiry@gmail>
2023-02-06 09:21:40 +0700

    chore(mobile): Add widgetbook example

--- packages/mobile/example/lib/main.dart
+++ packages/mobile/example/lib/main.dart
@@ -5,6 +5,7 @@ import 'component/avatar/avatar_example.dart';
 import 'component/button/zero_button_example.dart';
 import 'component/chip/chip_example.dart';
 import 'component/divider/divider_example.dart';
+import 'component/icon/icon_example.dart';
 import 'component/listtile/listile_example.dart';
 import 'component/rating/zero_rating_example.dart';
 import 'component/slider/zero_slider_example.dart';
@@ -193,6 +194,19 @@ class Examples extends StatelessWidget {
               );
             },
             text: 'Zero Switch Example',
+          ),
+          ZeroButton.primary(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const ZeroIconExample(),
+                ),
+              );
+            },
+            text: 'Zero Icon Example',
+          ),
+          const ZeroTwoToneIcons.upCircle(
+            color: Colors.blue,
           ),
         ],
       ),
