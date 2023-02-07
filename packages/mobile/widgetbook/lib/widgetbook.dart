499bf9890a5d884600d5faa49dde8760c4afefd0
Zulhijaya <80931803+zulhijaya18@users.noreply.github.com>
2023-02-07 14:59:06 +0800

    Feat/95 implement progress indicator mobile (#68)
    * feat: 95 implement progress indicator mobile
    
    * feat: 95 implement progress indicator mobile
    
    * feat: 95 implement progress indicator mobile
    
    * feat: 95 implement progress indicator mobile
    
    * feat: 95 implement progress indicator mobile
    
    * feat: 95 implement progress indicator mobile
    
    * feat: 95 implement progress indicator mobile

--- packages/mobile/widgetbook/lib/widgetbook.dart
+++ packages/mobile/widgetbook/lib/widgetbook.dart
@@ -66,6 +66,7 @@ class HotReload extends StatelessWidget {
             snackbarWidgetbookComponent,
             progressIndicatorWidgetbookComponent,
             datepickerWidgetbookComponent,
+            progressIndicatorWidgetbookComponent,
           ],
         ),
       ],
