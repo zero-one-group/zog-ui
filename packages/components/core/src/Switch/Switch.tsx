04a66df1c107a7f4a22ee9850045f7ddc749802a
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2023-02-07 13:00:39 +0700

    feat: add loading and disabled props

--- packages/components/core/src/Switch/Switch.tsx
+++ packages/components/core/src/Switch/Switch.tsx
@@ -1,5 +1,5 @@
 import * as RadixSwitch from '@radix-ui/react-switch';
-import { VariantProps } from '@stitches/react';
+import { keyframes, VariantProps } from '@stitches/react';
 import { ComponentProps, forwardRef } from 'react';
 import { styled } from '../stitches.config';
 
@@ -10,8 +10,8 @@ const getColorSchemeVariants = (colorScheme?: string) => {
       ? `$colors-${colorScheme}A7`
       : '$colors-blueA7',
     $$bgSwitchDisabled: colorScheme
-      ? `$colors-${colorScheme}A4`
-      : '$colors-blueA4',
+      ? `$colors-${colorScheme}A6`
+      : '$colors-blueA6',
   };
 };
 
@@ -22,51 +22,88 @@ const SwitchRoot = styled(RadixSwitch.Root, {
   position: 'relative',
   boxShadow: `0 2px 10px $blackA7`,
   WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)',
+  overflow: 'hidden',
+  width: 'auto',
+  color: 'white',
+  fontFamily: '$untitled',
   '&:focus': {
     boxShadow: `0 0 0 2px $$bgSwitchFocus`,
     transition: 'box-shadow 90ms',
     willChange: 'box-shadow',
   },
-  '&[data-state="checked"]': { backgroundColor: '$$bgSwitch' },
+  '&[data-state="checked"]': {
+    backgroundColor: '$$bgSwitch',
+    '&[data-disabled]': { backgroundColor: '$$bgSwitchDisabled' },
+  },
+  '&[data-disabled]': { backgroundColor: '$grayA6' },
   variants: {
     size: {
       sm: {
-        width: 28,
+        minWidth: 28,
         height: 16,
+        fontSize: '.5rem',
       },
       md: {
-        width: 44,
+        minWidth: 44,
         height: 22,
-      },
-    },
-    disabledVariant: {
-      true: {
-        backgroundColor: '$grayA6',
-        '&[data-state="checked"]': { backgroundColor: '$$bgSwitchDisabled' },
       },
     },
   },
 });
 
 const SwitchThumb = styled(RadixSwitch.Thumb, {
-  display: 'block',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  position: 'absolute',
   backgroundColor: 'white',
   borderRadius: '9999px',
   boxShadow: `0 2px 2px $blackA9`,
-  transition: 'transform 100ms',
-  transform: 'translateX(2px)',
-  willChange: 'transform',
+  top: '2px',
+  insetInlineStart: '2px',
+  transition: 'inset-inline-start 100ms',
+  '&[data-state="checked"]': {
+    '& div': {
+      borderTop: '1px solid $$bgSwitchDisabled',
+    },
+  },
+  '& div': {
+    borderTop: '1px solid $grayA7',
+  },
   variants: {
     size: {
       sm: {
         width: 12,
         height: 12,
-        '&[data-state="checked"]': { transform: 'translateX(14px)' },
+        '&[data-state="checked"]': { insetInlineStart: 'calc(100% - 14px)' },
       },
       md: {
         width: 18,
         height: 18,
-        '&[data-state="checked"]': { transform: 'translateX(24px)' },
+        '&[data-state="checked"]': { insetInlineStart: 'calc(100% - 20px)' },
+      },
+    },
+  },
+});
+
+const rotate = keyframes({
+  '0%': { transform: 'rotate(0deg)' },
+  '100%': { transform: 'rotate(360deg)' },
+});
+
+const StyledLoader = styled('div', {
+  borderRadius: '50%',
+  border: '1px solid white',
+  animation: `${rotate} 1s linear infinite`,
+  variants: {
+    size: {
+      sm: {
+        width: 8,
+        height: 8,
+      },
+      md: {
+        width: 12,
+        height: 12,
       },
     },
   },
@@ -74,6 +111,7 @@ const SwitchThumb = styled(RadixSwitch.Thumb, {
 
 export interface SwitchProps extends ComponentProps<typeof SwitchRoot> {
   disabled?: boolean;
+  loading?: boolean;
   colorScheme?: string;
   size?: Extract<VariantProps<typeof SwitchRoot>['size'], string>;
 }
@@ -83,7 +121,14 @@ export const Switch = forwardRef<
   SwitchProps
 >(
   (
-    { id, colorScheme, disabled = false, size = 'md', ...props }: SwitchProps,
+    {
+      id,
+      colorScheme,
+      disabled = false,
+      loading = false,
+      size = 'md',
+      ...props
+    },
     ref
   ) => {
     return (
@@ -92,11 +137,12 @@ export const Switch = forwardRef<
         ref={ref}
         css={{ ...getColorSchemeVariants(colorScheme) }}
         size={size}
-        disabled={disabled}
-        disabledVariant={disabled}
+        disabled={disabled || loading}
         {...props}
       >
-        <SwitchThumb size={size} />
+        <SwitchThumb size={size}>
+          {loading ? <StyledLoader size={size} /> : null}
+        </SwitchThumb>
       </SwitchRoot>
     );
   }
