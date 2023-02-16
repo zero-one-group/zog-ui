8f3e6d1b7ee289b9ad56ad0c3cc6e987ebb8476b
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2023-02-16 09:49:54 +0700

    feat: add basic table

--- packages/components/core/src/Table/Table.tsx
+++ packages/components/core/src/Table/Table.tsx
@@ -1,10 +1,82 @@
-/* eslint-disable-next-line */
-export interface TableProps {}
+import {
+  ColumnDef,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import { useState } from 'react';
+import { styled } from '../stitches.config';
 
-export function Table(props: TableProps) {
+const StyledTable = styled('table', {
+  width: '100%',
+  fontFamily: '$untitled',
+  fontSize: '14px',
+  border: 'none',
+  borderCollapse: 'collapse',
+  '& thead>tr>th, tbody>tr>td': {
+    padding: '1rem',
+  },
+  '& tbody>tr': {
+    borderBottom: '1px solid $gray4',
+    transition: 'background-color .1s linear',
+    '&:hover': {
+      backgroundColor: '$gray2',
+    },
+  },
+});
+
+const StyledTH = styled('th', {
+  fontWeight: 500,
+  textAlign: 'left',
+  backgroundColor: '$gray2',
+  borderBottom: '1px solid $gray4',
+});
+
+export interface TableProps<T> {
+  dataSource: T[];
+  columns: ColumnDef<T, unknown>[];
+}
+
+export function Table<T>({ dataSource, columns }: TableProps<T>) {
+  const [data, setData] = useState(() => [...dataSource]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   return (
     <div>
-      <h1>This is Table!</h1>
+      <StyledTable>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <tr key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <StyledTH key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </StyledTH>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </StyledTable>
     </div>
   );
 }
