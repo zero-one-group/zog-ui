3fa64b150d1b81bc8926de3ad94700f651341562
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2023-02-21 08:53:22 +0700

    feat: generate modal component

---
+++ packages/components/core/src/Modal/Modal.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ModalProps {}
+
+export function Modal(props: ModalProps) {
+  return (
+    <div>
+      <h1>Welcome to Modal!</h1>
+    </div>
+  );
+}
+
+export default Modal;
