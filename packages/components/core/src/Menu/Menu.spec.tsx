ec7b75a00698e18386f45908982e88cc0ec53508
Alfan Jauhari <hi@alfanjauhari.com>
2023-02-22 17:29:26 +0700

    test(menu): add simple test case for menu

--- packages/components/core/src/Menu/Menu.spec.tsx
+++ packages/components/core/src/Menu/Menu.spec.tsx
@@ -1,10 +1,43 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { Menu } from './Menu';
 
 describe('Menu', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Menu />);
+  it('should render successfully', async () => {
+    const { baseElement, getAllByRole, getByRole } = render(
+      <Menu title="Edit">
+        <Menu.Item title="Undo" onClick={() => console.log('Undo')} />
+        <Menu.Item title="Redo" disabled />
+        <Menu.Item title="Cut" />
+        <Menu title="Copy as">
+          <Menu.Item title="Text" />
+          <Menu.Item title="Video" />
+          <Menu title="Image">
+            <Menu.Item title=".png" />
+            <Menu.Item title=".jpg" />
+            <Menu.Item title=".svg" />
+            <Menu.Item title=".gif" />
+          </Menu>
+          <Menu.Item title="Audio" />
+        </Menu>
+        <Menu title="Share">
+          <Menu.Item title="Mail" />
+          <Menu.Item title="Instagram" />
+        </Menu>
+      </Menu>
+    );
+
+    const buttonMenu = getByRole('button', {
+      name: 'Edit',
+    });
+
+    expect(buttonMenu).toBeVisible();
+
+    await userEvent.click(buttonMenu);
+
+    expect(getAllByRole('menuitem')).toHaveLength(5);
+
     expect(baseElement).toBeTruthy();
   });
 });
