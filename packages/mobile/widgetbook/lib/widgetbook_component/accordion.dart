90c188d967e445d253f21878e1bb3219beef19b8
Muhammad Rijalul Kahfi <kahfi@zero-one-group.com>
2023-02-23 12:34:50 +0700

    feat: added widgetbook for accordion

--- packages/mobile/widgetbook/lib/widgetbook_component/accordion.dart
+++ packages/mobile/widgetbook/lib/widgetbook_component/accordion.dart
@@ -121,5 +121,65 @@ WidgetbookComponent accordionWidgetbookComponent = WidgetbookComponent(
             ),
           );
         }),
-  ],
+  WidgetbookUseCase(
+        name: 'Sub Tile (Nested)',
+        builder: (context) {
+          ZeroExpansionTile subChild = const ZeroExpansionTile(
+            title: Text('ZeroExpansionTile 1'),
+            subtitle: Text('Trailing expansion arrow icon'),
+            children: [
+              ListTile(title: Text('This is tile number 1')),
+              ListTile(title: Text('This is tile number 1')),
+            ],
+          );
+
+          ZeroExpansionTile child =  ZeroExpansionTile(
+            title: Text('ZeroExpansionTile 1'),
+            subtitle: Text('Trailing expansion arrow icon'),
+            children: [
+              subChild, subChild
+            ],
+          );
+
+          return PreviewWidget.builder(
+            builder: (context) => Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ZeroAccordion.underline(
+                leading: context.knobs
+                    .options(label: 'Leading', options: _iconsOption),
+                leadingRotateOnExpand: context.knobs.boolean(
+                    label: 'Leading Rotate on Expansion', initialValue: false),
+                trailing: context.knobs
+                    .options(label: 'Trailing', options: _iconsOption),
+                trailingRotateOnExpand: context.knobs.boolean(
+                    label: 'Trailing Rotate on Expansion', initialValue: true),
+                style: ZeroExpansionTileStyle.underline(
+                    iconColor: context.knobs.options(
+                        label: 'Icon Color', options: [...colorOptions]),
+                    collapsedIconColor: context.knobs.options(
+                        label: 'Collapsed Icon Color',
+                        options: [...colorOptions]),
+                    backgroundColor: context.knobs.options(
+                        label: 'backgroundColor Color',
+                        options: [
+                          const Option(label: 'White', value: ZeroColors.white),
+                          ...colorOptions
+                        ]),
+                    collapsedTextColor: context.knobs.options(
+                        label: 'Collapsed Text Color',
+                        options: [
+                          const Option(label: 'Black', value: ZeroColors.black),
+                          ...colorOptions
+                        ]),
+                    lineColor: context.knobs.options(
+                        label: 'Line Color', options: [...colorOptions]),
+                    collapsedLineColor: context.knobs
+                        .options(label: 'Collapsed Line Color', options: [...colorOptions]),
+                    childrenPadding: EdgeInsets.only(left: context.knobs.number(label: 'Children Left Padding', initialValue: 0.0) as double)),
+                children: [child, child, child],
+              ),
+            ),
+          );
+        }),
+    ],
 );
