af993ae2fbed41f45a1dc37c4434f6252ae57084
Zulhijaya <80931803+zulhijaya18@users.noreply.github.com>
2023-02-24 15:32:30 +0800

    Feat/154 implement grid mobile (#127)
    * 154: implement grid mobile

--- packages/mobile/example/lib/main.dart
+++ packages/mobile/example/lib/main.dart
@@ -395,7 +395,6 @@ class Examples extends StatelessWidget {
           onPressed: () {
             Navigator.of(context).push(
               MaterialPageRoute(
-<<<<<<< HEAD
                 builder: (context) => const ZeroGridExample(),
               ),
             );
@@ -406,8 +405,6 @@ class Examples extends StatelessWidget {
           onPressed: () {
             Navigator.of(context).push(
               MaterialPageRoute(
-=======
->>>>>>> 990e0e6 (feat(mobile): Refactor & implement global theme button group)
                 builder: (context) => const ZeroButtonGroupExample(),
               ),
             );
