d0b269604b83512f0e6b0471799e58b783da5991
vadhee <rivaldi@zero-one-group.com>
2023-03-01 14:24:25 +0800

    fix(zero-ui): fix aligment tabs with badge

--- packages/components/core/src/ProgressBar/ProgressBar.spec.tsx
+++ packages/components/core/src/ProgressBar/ProgressBar.spec.tsx
@@ -1,1 +1,10 @@
-// height: '100px',
+import { render } from '@testing-library/react';
+
+import ProgressBar from './ProgressBar';
+
+describe('ProgressBar', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<ProgressBar value={0} />);
+    expect(baseElement).toBeTruthy();
+  });
+});
