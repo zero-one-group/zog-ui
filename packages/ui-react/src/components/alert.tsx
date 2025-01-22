342a938f18e763e406393ed187df82f603d22887
khoerulumam <khoerul@zero-one-group.com>
2025-01-22 17:08:24 +0700

    fix generic types

--- packages/ui-react/src/components/alert.tsx
+++ packages/ui-react/src/components/alert.tsx
@@ -1,5 +1,6 @@
 import { type Assign } from '@ark-ui/react'
-import { type HTMLArkProps, ark } from '@ark-ui/react/factory'
+import type { HTMLArkProps, HTMLProps } from '@ark-ui/react/factory'
+import { ark } from '@ark-ui/react/factory'
 import { type AlertVariants, alertStyles } from '@repo/core-ui/alert.css'
 import * as React from 'react'
 
@@ -13,21 +14,20 @@ const Alert = React.forwardRef<HTMLDivElement, AlertProps>(
 )
 Alert.displayName = 'Alert'
 
-const AlertTitle = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLHeadingElement>>(
+const AlertTitle = React.forwardRef<HTMLHeadingElement, HTMLProps<'h5'>>(
   ({ className, ...props }, ref) => {
     const styles = alertStyles()
-    return <h5 ref={ref} className={styles.title({ className })} {...props} />
+    return <ark.h5 ref={ref} className={styles.title({ className })} {...props} />
   }
 )
 AlertTitle.displayName = 'AlertTitle'
 
-const AlertDescription = React.forwardRef<
-  HTMLParagraphElement,
-  React.HTMLAttributes<HTMLParagraphElement>
->(({ className, ...props }, ref) => {
-  const styles = alertStyles()
-  return <div ref={ref} className={styles.description({ className })} {...props} />
-})
+const AlertDescription = React.forwardRef<HTMLDivElement, HTMLProps<'div'>>(
+  ({ className, ...props }, ref) => {
+    const styles = alertStyles()
+    return <ark.div ref={ref} className={styles.description({ className })} {...props} />
+  }
+)
 AlertDescription.displayName = 'AlertDescription'
 
 export { Alert, AlertTitle, AlertDescription }
