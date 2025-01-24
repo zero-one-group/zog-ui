a43148490871c72b96cdc00fdc0bbefc8613ecbf
alifzeroone <alif@zero-one-group.com>
2025-01-24 11:25:28 +0700

    feat: 99 add storybook react breadcrumb

---
+++ apps/docs-react/stories/breadcrumb/breadcrumb.stories.tsx
@@ -0,0 +1,97 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+  BreadcrumbPage,
+  BreadcrumbEllipsis,
+} from '@repo/ui-react/breadcrumb'
+import React from 'react'
+import { Slash } from 'lucide-react'
+
+const meta: Meta = {
+  title: 'Basic Components/Breadcrumb',
+  component: Breadcrumb,
+}
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  render: (args) => (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href='/'>Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href='/components'>Components</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  ),
+}
+
+export const CustomSeparatorShowcase: Story = {
+  render: (args) => (
+    <Breadcrumb {...args}>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href='/'>Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator>
+          <Slash />
+        </BreadcrumbSeparator>
+        <BreadcrumbItem>
+          <BreadcrumbLink href='/components'>Components</BreadcrumbLink>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  ),
+}
+
+export const CollapsedShowcase: Story = {
+  render: (args) => (
+    <Breadcrumb {...args}>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href='/'>Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbEllipsis />
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href='/components'>Components</BreadcrumbLink>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  ),
+}
+
+export const CustomLinkShowcase: Story = {
+  render: (args) => (
+    <Breadcrumb {...args}>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <a href='/'>Home</a>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink href='/components'>Components</BreadcrumbLink>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  ),
+}
--- packages/core-ui/src/styles/breadcrumb.css.ts
+++ packages/core-ui/src/styles/breadcrumb.css.ts
@@ -3,12 +3,12 @@ import { type VariantProps, tv } from 'tailwind-variants'
 export const breadcrumbStyles = tv({
   slots: {
     base: '',
-    list: 'flex flex-wrap items-center gap-1.5 break-words text-muted-foreground text-sm sm:gap-2.5',
+    list: 'flex flex-wrap items-center items-stretch gap-1.5 break-words text-muted-foreground text-sm sm:gap-2.5',
     item: 'inline-flex items-center gap-1.5',
     link: 'transition-colors hover:text-foreground',
     page: 'font-normal text-foreground',
     separator: '[&>svg]:h-3.5 [&>svg]:w-3.5',
-    ellipsis: 'flex size-9 cursor-pointer items-center justify-center',
+    ellipsis: 'flex size-4 mx-1 cursor-pointer items-center justify-center',
     ellipsisIcon: 'size-4',
   },
 })
